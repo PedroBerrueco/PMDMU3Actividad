c971ee88ca93d316965954c0e5647bda9df86a45
Administrador <pedroberrueco@hotmail.com>
2023-12-03 18:43:28 +0100

    segundo commit

---
+++ app/src/main/java/com/pberrueco/pmdmu3actividad/TaskActivity.kt
@@ -0,0 +1,29 @@
+package com.pberrueco.pmdmu3actividad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pberrueco.pmdmu3actividad.databinding.ActivityMainBinding
+import com.pberrueco.pmdmu3actividad.databinding.ActivityTaskBinding
+
+class TaskActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityTaskBinding
+    private val binding: ActivityTaskBinding get() = _binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val user = intent.getStringExtra("user")
+
+        val taskFragment = TaskFragment().apply {
+            arguments = Bundle().apply {
+                putString("user", user)
+            }
+        }
+
+        // Agregar el fragmento al contenedor
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, taskFragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/pberrueco/pmdmu3actividad/TaskFragment.kt
@@ -0,0 +1,28 @@
+package com.pberrueco.pmdmu3actividad
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pberrueco.pmdmu3actividad.databinding.FragmentRegister2Binding
+import com.pberrueco.pmdmu3actividad.databinding.FragmentTaskBinding
+
+class TaskFragment : Fragment() {
+    private lateinit var _binding: FragmentTaskBinding
+    private val binding: FragmentTaskBinding get() = _binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val user = arguments?.getString("user")
+        binding.etTaskfrag.text = user
+    }
+}
